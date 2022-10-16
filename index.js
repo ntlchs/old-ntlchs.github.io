d9b221f2010e07be8067988f3dddc7bdf57d9893
ntlchs <nataliachies@gmail.com>
2022-10-16 18:02:22 -0300

    make js color changing border, image overlapping

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const backToTop = document.getElementById("backToTop");
 const scrollArea = document.getElementById("scroll_area");
 const windowSizeY = document.getElementById("firstScreen");
 const elementOffset = windowSizeY.offsetHeight;
+const presentationCard = document.getElementById("presentation_area");
 
 scrollArea.onscroll = () => {
   console.log(scrollArea.scrollTop);
@@ -13,4 +14,15 @@ scrollArea.onscroll = () => {
     backToTop.classList.add("class", "hidden");
     console.log("added class hidden");
   }
+};
+
+presentationCard.onclick = () => {
+  function changeColor(data) {
+    console.log(data);
+    presentationCard.style.borderColor = data.hex;
+  }
+  fetch("https://x-colors.herokuapp.com/api/random")
+    .then((response) => response.json())
+    .then((data) => changeColor(data))
+    .catch((err) => console.log(err));
 };
