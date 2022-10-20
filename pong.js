7b9c35f6ac174cf1780bbcd4b4d89068ed2239da
ntlchs <nataliachies@gmail.com>
2022-10-19 22:40:35 -0300

    make bar move with mouse, set barrier for bar not to overpass canvas

--- pong.js
+++ pong.js
@@ -1,16 +1,32 @@
 const pong = document.getElementById("pong");
 
 var s = function (c) {
-  var x = 100;
-  var y = 100;
+  let ballRadius = 50;
+  let barWidth = 180;
+  let barHeight = 30;
   var canvasWidth = (c.setup = function () {
     c.createCanvas(pong.clientWidth, pong.clientHeight);
   });
 
   c.draw = function () {
+    var ballX = 100;
+    var ballY = 100;
+    var barY = pong.clientHeight - 50;
+    var barrierLeft = pong.clientLeft + 20;
+    var barrierRight = pong.clientWidth - barWidth - 20;
+    var barX = c.mouseX;
+
+    if (barX < barrierLeft) {
+      barX = barrierLeft;
+    } else if (barX > barrierRight) {
+      barX = barrierRight;
+    }
     c.background(0);
     c.fill(255);
-    c.rect(x, y, 50, 50);
+    c.ellipse(ballX, ballY, ballRadius, ballRadius);
+    c.rect(barX, barY, barWidth, barHeight);
   };
 };
+
 var myp5 = new p5(s, "pong");
+pong.scrollIntoView();
