a232cb9ada29fe58b455d7b12315ba46eb1cf2b0
ntlchs <nataliachies@gmail.com>
2022-12-17 23:30:07 -0300

    basic design and some props

---
+++ src/scripts/backToTop.js
@@ -0,0 +1,10 @@
+// const scrollArea = document.getElementById("scroll_area");
+// const elementOffset = document.getElementById("firstScreen").offsetHeight;
+
+// scrollArea.onscroll = () => {
+//   if (scrollArea.scrollTop >= elementOffset - 1) {
+//     backToTop.classList.remove("class", "hidden");
+//   } else {
+//     backToTop.classList.add("class", "hidden");
+//   }
+// };
