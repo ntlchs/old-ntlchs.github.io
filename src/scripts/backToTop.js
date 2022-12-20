165ffd24e0453be8647344026fd7acd05bd24ce2
ntlchs <nataliachies@gmail.com>
2022-12-19 22:12:42 -0300

    add about me component, small adjustments

--- src/scripts/backToTop.js
+++ src/scripts/backToTop.js
@@ -1,10 +1,11 @@
-// const scrollArea = document.getElementById("scroll_area");
-// const elementOffset = document.getElementById("firstScreen").offsetHeight;
+const back_btn = document.getElementById("back_btn");
+const scrollArea = document.getElementById("pages");
+const elementOffset = document.getElementById("about").offsetHeight;
 
-// scrollArea.onscroll = () => {
-//   if (scrollArea.scrollTop >= elementOffset - 1) {
-//     backToTop.classList.remove("class", "hidden");
-//   } else {
-//     backToTop.classList.add("class", "hidden");
-//   }
-// };
+scrollArea.onscroll = () => {
+  if (scrollArea.scrollTop >= window.) {
+    back_btn.classList.remove("class", "hidden");
+  } else {
+    back_btn.classList.add("class", "hidden");
+  }
+};
