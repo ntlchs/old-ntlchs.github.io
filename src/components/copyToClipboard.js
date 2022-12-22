02a3a75bc711b48873ff1c2cc9984d59834ae847
ntlchs <nataliachies@gmail.com>
2022-12-22 10:57:48 -0300

    fix copy to clipboard script

--- src/components/copyToClipboard.js
+++ src/components/copyToClipboard.js
@@ -1,8 +1,15 @@
 const copyBtn = document.querySelector("#copy_btn");
 const content = document.querySelector("#copy_email");
+let text = content.innerText;
 
 copyBtn.addEventListener("click", () => {
-  let text = content.textContent;
-  navigator.clipboard.writeText(`${text}`);
-  alert(`Copied to clipboard!`);
+  setFocusToTextBox();
+  (async () => {
+    await parent.navigator.clipboard.writeText(text);
+    alert(`Copied to clipboard!`);
+  })();
 });
+
+function setFocusToTextBox() {
+  content.focus();
+}
