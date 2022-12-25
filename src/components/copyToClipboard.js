cce61d46ca7bd73b81b8063f2396ebe993303549
ntlchs <nataliachies@gmail.com>
2022-12-25 18:47:38 -0300

    minor fixes, add skills imgs, add pong page, make copy to clipboard better

--- src/components/copyToClipboard.js
+++ src/components/copyToClipboard.js
@@ -1,15 +1,27 @@
 const copyBtn = document.querySelector("#copy_btn");
 const content = document.querySelector("#copy_email");
 let text = content.innerText;
+const toast = document.querySelector("#copy_toast");
+let timeoutID = null;
 
 copyBtn.addEventListener("click", () => {
+  toast.classList.remove("hidden");
+  timeoutID = setTimeout(() => {
+    if (timeoutID !== null) {
+      toast.classList.add("hidden");
+    }
+    clearMessage();
+  }, 2000);
   setFocusToTextBox();
   (async () => {
     await parent.navigator.clipboard.writeText(text);
-    alert(`Copied to clipboard!`);
   })();
 });
 
 function setFocusToTextBox() {
   content.focus();
+}
+
+function clearMessage() {
+  clearTimeout(timeoutID);
 }
