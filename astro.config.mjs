6db22449dbd00fd7af2e2fec1a97ced3d06dae0d
ntlchs <nataliachies@gmail.com>
2023-02-16 04:42:39 -0300

    add tailwind, fix email sent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://ntlchs.github.io",
+  site: "https://ntlchs.github.io"
   //   base: '/my-repo',
+  ,
+  integrations: [tailwind()]
 });
