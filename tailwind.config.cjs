dbdcefd9279fcd6cce8307e3a56bbfcd58afd47f
ntlchs <nataliachies@gmail.com>
2023-02-23 04:04:35 -0300

    design changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "n-black": "#000",
+      "n-white": "#fffffc",
+      "n-red": "#ff2f00",
+      "n-green": "#1fa95b",
+      "n-yellow": "#fdc62e",
+      "n-purple": "#a39df7",
+      "n-pink": "#e339fc",
+      "n-lightgreen": "#b1ebd3",
+    },
+  },
+  plugins: [],
+};
