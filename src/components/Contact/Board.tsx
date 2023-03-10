623977aedbf4083baa159c50e8583b834fc96a42
ntlchs <nataliachies@gmail.com>
2023-03-10 18:16:22 -0300

    fix footer

--- src/components/Contact/Board.tsx
+++ src/components/Contact/Board.tsx
@@ -2,7 +2,7 @@ export default function ContactBoard() {
   return (
     <div
       id="contact_icons"
-      className="grid grid-cols-3 p-0 mx-auto my-0 box-border items-center mx-4 w-20 min-w-full"
+      className="grid grid-cols-3 p-0 my-0 box-border items-center mx-auto px-4 w-20 min-w-full gap-2"
     >
       <a id="github" href="https://github.com/ntlchs" target="_blank">
         <img src="/assets/github.svg" alt="GitHub" className="footer_icon" />
